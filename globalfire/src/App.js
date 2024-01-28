d81b916ad9c5ad3ecbfd095f7306f2ec74cd3050
AhShinPark_win <asinpark123@hotmail.com>
2024-01-29 04:32:01 +1300

    managed to load the 3d model to the page, give it more exposure and rotate

--- globalfire/src/App.js
+++ globalfire/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
+// import neilArmstrong from 'https://modelviewer.dev/shared-assets/models/NeilArmstrong.glb';
 import {Helmet} from 'react-helmet'
 // import globalFireLogo from "./logo/GlobalFire_orange_fire.glb";
 
 import './App.css';
 
+
 function Header() {
   return (
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
       <p>
         Edit <code>src/App.js</code> and save to reload.
       </p>
@@ -18,50 +20,66 @@ function Header() {
   );
 }
 
-function MetalBall(){
-  return(
+function NeilArmstrong() {
+  return (
     <div>
       <Helmet>
-        <script type="module" src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.4.0/model-viewer.min.js"></script>
+        <script
+          type="module"
+          src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.4.0/model-viewer.min.js"
+        ></script>
       </Helmet>
-      <div className='logoAnimation'>
-        <model-viewer camera-controls="" touch-action="pan-y" src="https://modelviewer.dev/shared-assets/models/NeilArmstrong.glb">
-        </model-viewer>
+
+      <div className="fixed-centre">
+        <model-viewer
+          src="https://modelviewer.dev/shared-assets/models/NeilArmstrong.glb"
+          auto-rotate
+          auto-rotate-delay="0"
+          rotation-per-second="30deg"
+          environment-image="https://neromotion.co.nz/assets/Uploads/test14.jpeg"
+          metalness="1"
+          roughness="0"
+          exposure="15"
+          ar-status="not-presenting"
+        ></model-viewer>
       </div>
     </div>
   );
 }
 
+// function MetalBall() {
+//   return (
+//     <div>
+//       <Helmet>
+//         <script
+//           type="module"
+//           src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.4.0/model-viewer.min.js"
+//         ></script>
+//       </Helmet>
 
-// function LogoAnimation (){
-//   return(
-//     <div className='logoAnimation'>
-//       <model-viewer roughness="0" metalness="1" id="model-viewer" powerpreference="low-power" reveal="auto" exposure="50" environment-image="./logo/background.jpeg" src={globalFireLogo} alt="NM 3D Logo" auto-rotate="" auto-rotate-delay="0" rotation-per-second="30deg" ar-status="not-presenting">
-//       </model-viewer>
+//       <div className="fixed-centre">
+//         <model-viewer
+//           src="https://modelviewer.dev/shared-assets/models/reflective-sphere.gltf"
+//           auto-rotate
+//           auto-rotate-delay="0"
+//           rotation-per-second="30deg"
+//           environment-image="https://neromotion.co.nz/assets/Uploads/test14.jpeg"
+//           metalness="1"
+//           roughness="0"
+//           ar-status="not-presenting"
+//         ></model-viewer>
+//       </div>
 //     </div>
 //   );
 // }
 
-// function Header() {
-//   return (
-//     <header className="App-header">
-//       <img src={logo} className="App-logo" alt="GlobalFire-logo" />
-//       <p>
-//         Edit <code>src/App.js</code> and save to reload.
-//       </p>
-//       <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-//         Learn React
-//       </a>
-//     </header>
-//   );
-// }
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MetalBall />
-      {/* <LogoAnimation /> */}
+      <NeilArmstrong />
+      {/* <MetalBall /> */}
     </div>
   );
 }
