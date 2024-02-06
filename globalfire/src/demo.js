fb99d5116349fb17e768e7597a601c4ee2b206a8
AhShinPark_win <asinpark123@hotmail.com>
2024-02-07 01:01:41 +1300

    trying to get the scroll event to work every time I scroll

--- globalfire/src/demo.js
+++ globalfire/src/demo.js
@@ -1,32 +1,37 @@
-const scroller = new LocomotiveScroll({
+let scroller = new LocomotiveScroll({
     el: document.querySelector("[data-scroll-container]"),
     smooth: true,
 });
 
-function navbarChange(){
-    const header = document.querySelector("header");
-    const navbar = document.querySelector("nav");
-    const logo = document.querySelector("#logo");
-    let y = header.scrollTop();
-    
-    if (y > 0){
-        header.classList.add("header-flat");
-        header.classList.remove("header");
+let headerTop = document.querySelector("header");
+let navbar = document.querySelector("nav");
+let logo = document.querySelector("#logo");
+let section1 = document.querySelector("#section1");
+let positionY
 
+function updatePositionY(){
+    positionY = window.scrollY;
+    
+    if (positionY > 0){
+        headerTop.classList.add("header-flat");
+        headerTop.classList.remove("header");
+        
         navbar.classList.add("navbar-flat");
         navbar.classList.remove("navbar");
         
         logo.classList.add("logo-flat");
         logo.classList.remove("logo");
     }
-    else if (y <= 10){
-        header.classList.add("header");
-        header.classList.remove("header-flat");
-
+    else if (positionY <= 10){
+        headerTop.classList.add("header");
+        headerTop.classList.remove("header-flat");
+        
         navbar.classList.add("navbar");
         navbar.classList.remove("navbar-flat");
-
+        
         logo.classList.add("logo");
         logo.classList.remove("logo-flat");
     }
 }
+
+let updateY = () => {headerTop.addEventListener("scroll",updatePositionY())}
