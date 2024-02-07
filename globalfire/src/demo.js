2bd9ef3f930d71bd5489f461170d6ba32aaef63e
AhShinPark_mac <asinpark123@hotmail.com>
2024-02-07 17:25:46 +1300

    got the function working correctly need to get it to work upon scrolling past a certain y position

--- globalfire/src/demo.js
+++ globalfire/src/demo.js
@@ -1,37 +1,34 @@
-let scroller = new LocomotiveScroll({
-    el: document.querySelector("[data-scroll-container]"),
-    smooth: true,
-});
+// let scroller = new LocomotiveScroll({
+//     el: document.querySelector("[data-scroll-container]"),
+//     smooth: true,
+// });
 
-let headerTop = document.querySelector("header");
-let navbar = document.querySelector("nav");
-let logo = document.querySelector("#logo");
-let section1 = document.querySelector("#section1");
-let positionY
+function updatePositionY() {
+    let headerTop = document.querySelector("header");
+    let navbar = document.querySelector("nav");
+    let logo = document.querySelector("#logo");
+    // let section1 = document.querySelector("#section1");
 
-function updatePositionY(){
-    positionY = window.scrollY;
-    
-    if (positionY > 0){
+    if (window.scrollY > 90) {
         headerTop.classList.add("header-flat");
         headerTop.classList.remove("header");
-        
+
         navbar.classList.add("navbar-flat");
         navbar.classList.remove("navbar");
-        
+
         logo.classList.add("logo-flat");
         logo.classList.remove("logo");
-    }
-    else if (positionY <= 10){
+    } else if (window.scrollY < 90) {
         headerTop.classList.add("header");
         headerTop.classList.remove("header-flat");
-        
+
         navbar.classList.add("navbar");
         navbar.classList.remove("navbar-flat");
-        
+
         logo.classList.add("logo");
         logo.classList.remove("logo-flat");
     }
-}
+};
 
-let updateY = () => {headerTop.addEventListener("scroll",updatePositionY())}
+
+window.onscroll = updatePositionY();
