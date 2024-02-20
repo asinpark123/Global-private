310b0f2feb35e13fea996424b83fa130308471e7
AhShinPark_mac <asinpark123@hotmail.com>
2024-02-20 17:51:06 +1300

    touch detection works need to make all elements opacity to 1 where it needs to be for touch devices

--- globalfire/src/demo.js
+++ globalfire/src/demo.js
@@ -2,27 +2,26 @@ function navbarChange() {
     let headerTop = document.querySelector("header");
     let navbar = document.querySelector("nav");
     let logo = document.querySelector("#logo");
+	if (window.scrollY > 200) {
+		headerTop.classList.add("header-flat");
+		headerTop.classList.remove("header");
 
-    if (window.scrollY > 200) {
-        headerTop.classList.add("header-flat");
-        headerTop.classList.remove("header");
+		navbar.classList.add("navbar-flat");
+		navbar.classList.remove("navbar");
 
-        navbar.classList.add("navbar-flat");
-        navbar.classList.remove("navbar");
+		logo.classList.add("logo-flat");
+		logo.classList.remove("logo");
+	} else if (window.scrollY < 200) {
+		headerTop.classList.add("header");
+		headerTop.classList.remove("header-flat");
 
-        logo.classList.add("logo-flat");
-        logo.classList.remove("logo");
-    } else if (window.scrollY < 200) {
-        headerTop.classList.add("header");
-        headerTop.classList.remove("header-flat");
+		navbar.classList.add("navbar");
+		navbar.classList.remove("navbar-flat");
 
-        navbar.classList.add("navbar");
-        navbar.classList.remove("navbar-flat");
-
-        logo.classList.add("logo");
-        logo.classList.remove("logo-flat");
-    }
-};
+		logo.classList.add("logo");
+		logo.classList.remove("logo-flat");
+	}
+}
 
 // function navbarBGActivate() {
 //     let headerFlat = document.querySelector(".header-flat")
@@ -30,38 +29,38 @@ function navbarChange() {
 //         headerFlat.style.backgroundImage = "none";
 //     } else if (window.scrollY > 200) {
 //         headerFlat.style.backgroundImage = "linear-gradient(black, rgb(20,20,20), transparent";
-//     } 
+//     }
 // };
 
 function headerBGfade() {
-    if (window.scrollY < 100) {
-        $(".headerBG-fade").fadeOut(500);
-    } else if (window.scrollY > 100) {
-        $(".headerBG-fade").fadeIn(500);
-    } 
-};
+	if (window.scrollY < 100) {
+		$(".headerBG-fade").fadeOut(500);
+	} else if (window.scrollY > 100) {
+		$(".headerBG-fade").fadeIn(500);
+	}
+}
 
 let services = document.getElementsByClassName("services");
 let servicesSection = document.getElementById("Services");
-for(let i = 0; i < services.length; i++){
-    services[i].addEventListener('mouseenter', function(){
-        services[i].style.opacity = 1;
-        services[i].style.pointerEvents = "none";
-        services[i].style.transitionDelay= '0.0s';
-        services[i].style.transition = "0.0s";
-    })
-    services[i].addEventListener('mouseleave', function(){
-        services[i].style.opacity = 0.4;
-        services[i].style.pointerEvents = "auto";
-        services[i].style.transitionDelay = "0.2s";
-        services[i].style.transition = "2s";
-    })
-    servicesSection.addEventListener("mouseenter", function () {
-        services[i].style.opacity = 0.4;
-        services[i].style.pointerEvents = "auto";
-        services[i].style.transitionDelay = "0.2s";
-        services[i].style.transition = "2s";
-    });
+for (let i = 0; i < services.length; i++) {
+	services[i].addEventListener("mouseenter", function () {
+		services[i].style.opacity = 1;
+		services[i].style.pointerEvents = "none";
+		services[i].style.transitionDelay = "0.0s";
+		services[i].style.transition = "0.0s";
+	});
+	services[i].addEventListener("mouseleave", function () {
+		services[i].style.opacity = 0.4;
+		services[i].style.pointerEvents = "auto";
+		services[i].style.transitionDelay = "0.2s";
+		services[i].style.transition = "2s";
+	});
+	servicesSection.addEventListener("mouseenter", function () {
+		services[i].style.opacity = 0.4;
+		services[i].style.pointerEvents = "auto";
+		services[i].style.transitionDelay = "0.2s";
+		services[i].style.transition = "2s";
+	});
 }
 
 // let scroller = new LocomotiveScroll({
@@ -70,71 +69,76 @@ for(let i = 0; i < services.length; i++){
 // });
 
 let items = document.querySelectorAll(".owl-carousel .item");
-for(let i = 0; i < items.length; i++){
-    items[i].addEventListener('mouseenter', function(){
-        items[i].style.opacity = 1;
-        items[i].style.pointerEvents = "none";
-        items[i].style.transitionDelay= '0.0s';
-        items[i].style.transition = "0.0s";
-    })
-    items[i].addEventListener('mouseleave', function(){
-        items[i].style.opacity = 0.8;
-        items[i].style.pointerEvents = "auto";
-        items[i].style.transitionDelay = "0.2s";
-        items[i].style.transition = "5s";
-    })
+
+for (let i = 0; i < items.length; i++) {
+	items[i].addEventListener("mouseenter", function () {
+		items[i].style.opacity = 1;
+		items[i].style.pointerEvents = "none";
+		items[i].style.transitionDelay = "0.0s";
+		items[i].style.transition = "0.0s";
+	});
+	items[i].addEventListener("mouseleave", function () {
+		items[i].style.opacity = 0.8;
+		items[i].style.pointerEvents = "auto";
+		items[i].style.transitionDelay = "0.2s";
+		items[i].style.transition = "5s";
+	});
 }
 
 // Owl Carousel 2
 $(document).ready(function () {
-    let owl = $(".owl-carousel");
-    owl.owlCarousel({
-        // stagePadding: 50,
-        loop: true,
-        center: true,
-        margin: 10,
-        autoWidth: true,
-        autoplay: true,
-        autoplayTimeout: 5000,
-        autoplaySpeed: 1000,
-        // autoplayHoverPause: true,
-    });
+	let owl = $(".owl-carousel");
+	owl.owlCarousel({
+		// stagePadding: 50,
+		loop: true,
+		center: true,
+		margin: 10,
+		autoWidth: true,
+		autoplay: true,
+		autoplayTimeout: 5000,
+		autoplaySpeed: 1000,
+		// autoplayHoverPause: true,
+	});
 });
 
 // simpleParallax 5.6.2
 $(document).ready(function () {
-    let carouselImages = document.querySelectorAll(".owl-carousel .imgWindow img");
-    // let glImage = document.querySelectorAll(".glInfo .imageContainer img");
-    new simpleParallax(carouselImages,{
-        scale: 1.3
-    });
-    // new simpleParallax(glImage, {
-    //     scale: 1.3,
-    // });
+	let carouselImages = document.querySelectorAll(
+		".owl-carousel .imgWindow img"
+	);
+	// let glImage = document.querySelectorAll(".glInfo .imageContainer img");
+	new simpleParallax(carouselImages, {
+		scale: 1.3,
+	});
+	// new simpleParallax(glImage, {
+	//     scale: 1.3,
+	// });
 });
 
 // Magnific Popup
 $(document).ready(function () {
-    let popup = $(".popup-container")
-    popup.magnificPopup({
-        delegate: "a",
-        type: "image",
-        gallery: { enabled: true },
-        image: {
-            titleSrc: 'title',
-            verticalFit: true,
-            tError: '<a href="%url%">The image</a> could not be loaded.',
-        },
-        mainClass: "mfp-with-zoom", // this class is for CSS animation below
-        zoom: {
-            enabled: true, // By default it's false, so don't forget to enable it
-            duration: 300, // duration of the effect, in milliseconds
-            easing: "ease-in-out", // CSS transition easing function
-            opener: function (openerElement) {
-                return openerElement.is("img") ? openerElement : openerElement.find("img");
-            },
-        },
-    });
+	let popup = $(".popup-container");
+	popup.magnificPopup({
+		delegate: "a",
+		type: "image",
+		gallery: { enabled: true },
+		image: {
+			titleSrc: "title",
+			verticalFit: true,
+			tError: '<a href="%url%">The image</a> could not be loaded.',
+		},
+		mainClass: "mfp-with-zoom", // this class is for CSS animation below
+		zoom: {
+			enabled: true, // By default it's false, so don't forget to enable it
+			duration: 300, // duration of the effect, in milliseconds
+			easing: "ease-in-out", // CSS transition easing function
+			opener: function (openerElement) {
+				return openerElement.is("img")
+					? openerElement
+					: openerElement.find("img");
+			},
+		},
+	});
 });
 
 // Teams Tab
@@ -150,7 +154,7 @@ function openTab(evt, cityName) {
 	}
 	document.getElementById(cityName).style.display = "block";
 	evt.currentTarget.className += " active";
-    AOS.refresh();
+	AOS.refresh();
 }
 document.getElementById("defaultOpen").click();
 
@@ -160,42 +164,73 @@ let teamData, i, j, photoSrc, staffName, jobTitle, title;
 fetch("./people.json")
 	.then((response) => response.json())
 	.then((json) => {
-        teamData = json;
-        for(i = 0;i<teamData.length;i++){
-            title = teamData[i].team;
-            const $jobCategory = $('<div class="jobCategoryContainer"></div>');
-            const $jobCategoryTitle = $(`<h4 class="jobCategory">${title}</h4>`);
-            const $staffContainer = $('<div class="staffContainer"></div>');
-            for(j = 0; j<teamData[i].list.length;j++){
-                const $staff = $(`<div class="staff" data-aos="fade-right" data-aos-delay="${j*200}"></div>`);
-                $staffContainer.append($staff);
-                photoSrc = teamData[i].list[j].photo;
-                staffName = teamData[i].list[j].name;
-                jobTitle = teamData[i].list[j].duty;
-                const $staffPhoto = $(`<div class=imageContainer><img src="${photoSrc}" alt="${staffName}"></div>`);
-                const $staffInfo = $('<div class="staffInfo"></div>');
-                $staffPhoto.append($staffInfo);
-                $staff.append($staffPhoto);
-                const $staffName = $(`<div class="name">${staffName}</div>`);
-                const $staffJobTitle = $(`<div class="jobTitle"><i>${jobTitle}</i></div>`);
-                $staffInfo.append($staffName,$staffJobTitle)
-            }
-            $jobCategory.append($jobCategoryTitle, $staffContainer);
-            $(".tabContent").append($jobCategory);
-        }
-        AOS.init();
-});
+		teamData = json;
+		for (i = 0; i < teamData.length; i++) {
+			title = teamData[i].team;
+			const $jobCategory = $('<div class="jobCategoryContainer"></div>');
+			const $jobCategoryTitle = $(`<h4 class="jobCategory">${title}</h4>`);
+			const $staffContainer = $('<div class="staffContainer"></div>');
+			for (j = 0; j < teamData[i].list.length; j++) {
+				const $staff = $(
+					`<div class="staff" data-aos="fade-right" data-aos-delay="${
+						j * 200
+					}"></div>`
+				);
+				$staffContainer.append($staff);
+				photoSrc = teamData[i].list[j].photo;
+				staffName = teamData[i].list[j].name;
+				jobTitle = teamData[i].list[j].duty;
+				const $staffPhoto = $(
+					`<div class=imageContainer><img src="${photoSrc}" alt="${staffName}"></div>`
+				);
+				const $staffInfo = $('<div class="staffInfo"></div>');
+				$staffPhoto.append($staffInfo);
+				$staff.append($staffPhoto);
+				const $staffName = $(`<div class="name">${staffName}</div>`);
+				const $staffJobTitle = $(
+					`<div class="jobTitle"><i>${jobTitle}</i></div>`
+				);
+				$staffInfo.append($staffName, $staffJobTitle);
+			}
+			$jobCategory.append($jobCategoryTitle, $staffContainer);
+			$(".tabContent").append($jobCategory);
+		}
+		AOS.init();
+	});
 
 menu = document.getElementById("navbar");
 hamburgerButton = document.querySelector(".hamburger");
 closeButton = document.querySelector(".hamburgerClose img");
-function hamburgerMenuOpen(){
-    menu.classList.add('mobile')
-    closeButton.classList.add('active')
-    hamburgerButton.classList.add('active')
+function hamburgerMenuOpen() {
+	menu.classList.add("mobile");
+	closeButton.classList.add("active");
+	hamburgerButton.classList.add("active");
 }
-function hamburgerMenuClose(){
-    menu.classList.remove('mobile');
-    closeButton.classList.remove('active')
-    hamburgerButton.classList.remove('active')
+function hamburgerMenuClose() {
+	menu.classList.remove("mobile");
+	closeButton.classList.remove("active");
+	hamburgerButton.classList.remove("active");
+}
+
+function isTouchDevice() {
+	return (
+		"ontouchstart" in window ||
+		navigator.maxTouchPoints > 0 ||
+		navigator.msMaxTouchPoints > 0
+	);
+}
+let touchDetection;
+function detectTouchOnLoad(){
+    touchDetection = isTouchDevice();
+    let i
+    if (!touchDetection){
+        canvas.style.zIndex = -1;
+        for(i = 0; i< services.length; i++){
+            services[i].classList.add("touch");
+        }
+        console.log("is touch device")
+    }
+    else{
+        console.log("is NOT touch device")
+    }
 }
